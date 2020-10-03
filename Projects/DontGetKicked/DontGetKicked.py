e6e2c1f72707f079ed47cbd420e99215bb3b147d
Guruprasad Kamath <guru241987@gmail.com>
2020-10-03 10:25:41 +0200

    Data Load for DontGetKicked

--- Projects/DontGetKicked/DontGetKicked.py
+++ Projects/DontGetKicked/DontGetKicked.py
@@ -1,5 +1,10 @@
 #! /usr/bin/python3
-#import sys
-#print(sys.path)
 
-from MLPipeline import utils
+import os, sys
+from MLPipeline.DtProcess import DataLoader
+
+
+dl = DataLoader(os.path.join(sys.path[0],'Data'),'training.csv','test.csv')
+train_data, test_data = dl.load()
+
+print(train_data.head())
